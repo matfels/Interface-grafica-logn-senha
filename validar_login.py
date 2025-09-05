fc0502afa536499b504c4f7b8f7a336136e66bee
Matheus Melo <162399194+matfels@users.noreply.github.com>
2025-09-05 11:26:30 -0300

    Login e senha com interface gráfica
    Login e senha com interface gráfica pronta

---
+++ validar_login.py
@@ -0,0 +1,7 @@
+def login(usuario, senha):
+    if usuario == "123" and senha == '123':
+        print('deu certo')
+        return "Login realizado com sucesso!"
+
+    else:
+        return "Falha no Login!"
